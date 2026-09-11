4a046466b8ea7097c3118185d600e1fbb518961b
MrHacker061 <MrHacker061@users.noreply.github.com>
2026-09-10 19:58:47 -0600

    Add safe Nmap XML evidence parser

--- redpath/__init__.py
+++ redpath/__init__.py
@@ -2,3 +2,6 @@
 
 __version__ = "0.1.0"
 
+from redpath.nmap_parser import NmapImportError, NmapImportResult, parse_nmap_xml_bytes, parse_nmap_xml_file
+
+__all__ = ["NmapImportError", "NmapImportResult", "parse_nmap_xml_bytes", "parse_nmap_xml_file"]
